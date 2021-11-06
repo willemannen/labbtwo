f8c76dac1e7aaac80af83b30bc85bffb3c485758
willemannen <brwi20vx@student.ju.se>
2021-11-06 11:50:35 +0100

    Initial Commit

---
+++ labbtwo/labbtwoApp.swift
@@ -0,0 +1,17 @@
+//
+//  labbtwoApp.swift
+//  labbtwo
+//
+//  Created by William Branth on 2021-11-06.
+//
+
+import SwiftUI
+
+@main
+struct labbtwoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
